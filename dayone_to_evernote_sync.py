ab56799e2c4ff6984b749c41ba8be0851df4628f
sosukeinu <jonathanbatteas@gmail.com>
2012-11-23 16:28:39 -0500

    Added basic limit checking
    to limit number of synced notes to no more than 250 per day. This is because Evernote has a limit of 250 emails per day for premium users. This is a quick fix, and better checking should be done, like asking the user if they are a free or premium user, and a default value for sync limit in the absence of user input.

--- dayone_to_evernote_sync.py
+++ dayone_to_evernote_sync.py
@@ -1,5 +1,5 @@
 #import modules
-import os, sys, inspect, glob, re, fnmatch, getpass, smtplib, plistlib, unicodedata, codecs
+import os, sys, inspect, glob, re, fnmatch, getpass, smtplib, plistlib, unicodedata, codecs, datetime, time
 # sys.setdefaultencoding is cancelled by site.py
 reload(sys)    # to re-enable sys.setdefaultencoding()
 sys.setdefaultencoding('utf-8')
@@ -29,6 +29,9 @@ def file_exists(filename):
     except:
         return False
 
+now = datetime.datetime.now()
+today = now.strftime("%m/%d/%Y")
+processed = 0
 #look for sync_list.xml in the same directory as this script. if it is not found, ask for user input
 if file_exists('sync_list.xml'):
     print ("sync_list.xml exists. Using information stored there.")   #TEST TO BE REMOVED
@@ -100,8 +103,8 @@ else:
     synced = ET.SubElement(root, "synced")
     synced.set("name", "Already Synced")
     sfile = ET.SubElement(synced, "sfile")
-    sfile.set("name", "Filename")
-    sfile.text = "base"
+    sfile.set("date", today)
+    sfile.text = "firstsync"
 
     tree = ET.ElementTree(root)
     tree.write("sync_list.xml")
@@ -124,8 +127,19 @@ for elem in root.findall('entags'):
     entags = elem.text
 for elem in root.findall('sfile'):
     synced = elem.text
+dcheck = 0
+for elem in root.findall('synced/sfile'):
+  fdate = elem.attrib.get('date')
+  if fdate == today:
+    dcheck += 1
 #if sync_list.xml exists get the list of (UUIDs) $entries that have already been synced, and exclude them from the current query. If no UUID's exist in sync_list.xml, ignore
-
+synclimit = 250 - dcheck #default
+print "dcheck " + str(dcheck)
+print "synclimit " + str(synclimit)
+if synclimit == 0:
+    print "Sorry, you've reached your limit for file syncing today. The limit is reset by Evernote each night at 12:00 a.m."
+    sys.exit()
+synclimit = int(raw_input("How many files do you want to sync today? You have a max amount of " + str(synclimit) + " left today: "))
 #get the list of all file names (UUID#.doentry) within var filepath that are NOT listed in sync_list.xml, if sync_list.xml does not exist, get all files (*.doentry) in var filepath
     #FOR EACH file name, open as xml document and get the data
 tree = ET.parse("sync_list.xml")
@@ -133,109 +147,126 @@ tree = ET.parse("sync_list.xml")
 root = tree.getroot()
 synced = [elt.text for elt in root.findall('synced/sfile')]
 for filename in os.listdir(filepath):
-    if fnmatch.fnmatch(filename, '*.doentry') and filename not in synced:
-        filename = os.path.join(filepath, filename)
-#Thanks Martijn Pieters http://stackoverflow.com/users/100297/martijn-pieters
-        result = plistlib.readPlist(filename)
-        t = result['Creation Date']
-        docreationdate = t.strftime('%m/%d/%Y') #ADDED
-        entry = result['Entry Text']
-        donotecontent = markdown2.markdown(entry)
-        if result['Starred'] == True:
-          dostarred = "#DayOne Starred"
-        else:
-          dostarred = ""
-        dofilename = result['UUID'] + ".doentry"
-        if result.get('Weather', None):
-          weather = result['Weather']
-        else:
-            weather = ""
-        if result.get('Celsius', None):
-          dotempcel = weather['Celsius'] + '&#xb0;'
-        else:
-            dotempcel = ""
-        if result.get('Description', None):
-          doweatherdesc = weather['Description']
-        else:
-            doweatherdesc = ""
-        if result.get('Fahrenheit', None):
-          dotempfah = weather['Fahrenheit'] + '&#xb0;'
-        else:
-          dotempfah = ""
-        if result.get('Location', None):
-          location = result['Location']
-        else:
-            location = ""
-        if result.get('Administrative Area', None):
-          dolocarea = location['Administrative Area'] + ', '
-        else:
-            dolocarea =""
-        if result.get('Country', None):
-          doloccountry = location['Country']
-        else:
-            doloccountry = ""
-        if result.get('Locality', None):
-          doloccity = location['Locality'] + ', '
-        else:
-            doloccity = ""
-        if result.get('Longitude', None):
-          doloclong = location['Longitude']
-        else:
-            doloclong = ""
-        if result.get('Latitude', None):
-          doloclat = location['Latitude']
-        else:
-            doloclat = ""
-        if result.get('Place Name', None):
-          dolocadd = location['Place Name'] + ', '
-        else:
-            dolocadd = ""
-          
-        #Thanks to RocketDonkey http://stackoverflow.com/users/1009277/rocketdonkey
-        if result.get('Tags', None):
-            dtags = result["Tags"]
-            dotags = '#' + ' #'.join(dtags)
-        else:
-            dotags = ""
-        if result.get('Time Zone', None):
-            dotimezone = result["Time Zone"]
-        else:
-            dotimezone = ""
-            
-        #create an email
-            # SEND TO var enmail
+    if processed >= synclimit:
+        print "You've successfully synced " + str(synclimit) + " files."
+        sys.exit()
+    else:
+        if fnmatch.fnmatch(filename, '*.doentry') and filename not in synced:
+            filename = os.path.join(filepath, filename)
+    #Thanks Martijn Pieters http://stackoverflow.com/users/100297/martijn-pieters
+            result = plistlib.readPlist(filename)
+            t = result['Creation Date']
+            docreationdate = t.strftime('%m/%d/%Y') #ADDED
+            entry = result['Entry Text']
+            donotecontent = markdown2.markdown(entry)
+            if result['Starred'] == True:
+              dostarred = "#DayOne Starred"
+            else:
+              dostarred = ""
+            dofilename = result['UUID'] + ".doentry"
+            if result.get('Weather', None):
+              weather = result['Weather']
+            else:
+                weather = ""
+            if result.get('Celsius', None):
+              dotempcel = weather['Celsius'] + '&#xb0;'
+            else:
+                dotempcel = ""
+            if result.get('Description', None):
+              doweatherdesc = weather['Description']
+            else:
+                doweatherdesc = ""
+            if result.get('Fahrenheit', None):
+              dotempfah = weather['Fahrenheit'] + '&#xb0;'
+            else:
+              dotempfah = ""
+            if result.get('Location', None):
+              location = result['Location']
+            else:
+                location = ""
+            if result.get('Administrative Area', None):
+              dolocarea = location['Administrative Area'] + ', '
+            else:
+                dolocarea =""
+            if result.get('Country', None):
+              doloccountry = location['Country']
+            else:
+                doloccountry = ""
+            if result.get('Locality', None):
+              doloccity = location['Locality'] + ', '
+            else:
+                doloccity = ""
+            if result.get('Longitude', None):
+              doloclong = location['Longitude']
+            else:
+                doloclong = ""
+            if result.get('Latitude', None):
+              doloclat = location['Latitude']
+            else:
+                doloclat = ""
+            if result.get('Place Name', None):
+              dolocadd = location['Place Name'] + ', '
+            else:
+                dolocadd = ""
+              
+            #Thanks to RocketDonkey http://stackoverflow.com/users/1009277/rocketdonkey
+            if result.get('Tags', None):
+                dtags = result["Tags"]
+                dotags = '#' + ' #'.join(dtags)
+            else:
+                dotags = ""
+            if result.get('Time Zone', None):
+                dotimezone = result["Time Zone"]
+            else:
+                dotimezone = ""
+                
+            #create an email
+                # SEND TO var enmail
 
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = 'Day One Entry: ' + docreationdate + ' ' + ennotebook + ' ' + entags + ' ' + dotags + ' ' + dostarred
-        msg['From'] = gmailadd
-        msg['To'] = enmail
-        html = donotecontent + '<br /><br />' + dotempfah + ' ' + doweatherdesc + '<br /><br />' + dolocadd + doloccity + dolocarea + doloccountry
-        part1 = MIMEText(html.encode('utf-8'), 'html', 'utf-8')
-        msg.attach(part1)
-            # SENT FROM var gmailadd
-            # SMTP pass var gmpass
-        gmail_pwd = gmpass
-        smtpserver = smtplib.SMTP("smtp.gmail.com",587)
-        smtpserver.ehlo()
-        smtpserver.starttls()
-        smtpserver.ehlo
-        smtpserver.login(gmailadd, gmail_pwd)
-            # SUBJECT Day One Journal Entry: var docreationdate, if var ennotebook is not empty @ennotebook, if var entags is not empty, explode list of each individual tag as #tag1 #tag2 #tag3, if var dostarred exists #dostarred
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = 'Day One Entry: ' + docreationdate + ' ' + ennotebook + ' ' + entags + ' ' + dotags + ' ' + dostarred
+            msg['From'] = gmailadd
+            msg['To'] = enmail
+            html = donotecontent + '<br /><br />' + dotempfah + ' ' + doweatherdesc + '<br /><br />' + dolocadd + doloccity + dolocarea + doloccountry
+            part1 = MIMEText(html.encode('utf-8'), 'html', 'utf-8')
+            msg.attach(part1)
+                # SENT FROM var gmailadd
+                # SMTP pass var gmpass
+            gmail_pwd = gmpass
+            smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+            smtpserver.ehlo()
+            smtpserver.starttls()
+            smtpserver.ehlo
+            smtpserver.login(gmailadd, gmail_pwd)
+                # SUBJECT Day One Journal Entry: var docreationdate, if var ennotebook is not empty @ennotebook, if var entags is not empty, explode list of each individual tag as #tag1 #tag2 #tag3, if var dostarred exists #dostarred
 
-            #Send email
-        smtpserver.sendmail(gmailadd, enmail, msg.as_string())
-        smtpserver.quit()
+                #Send email
+            # sent = 0
+            # while sent < 3:
+            #     try:
+            #         smtpserver.sendmail(gmailadd, enmail, msg.as_string())
+            #         smtpserver.quit()
+            #     except Exception, e:
+            #         print "Unable to send email. Error: %s" % str(e)
+            #         print "Waiting one minute before trying again."
+            #         time.sleep(60)
+            #         sent += 1
+            # continue
+            smtpserver.sendmail(gmailadd, enmail, msg.as_string())
+            smtpserver.quit()
 
-        #Add dofilename to sync_list.xml as incremented <key>UUID#</key>
-        #Thanks to J.F. Sebastian http://stackoverflow.com/users/4279/j-f-sebastian
-        tree = ET.parse('sync_list.xml')
-        synced = tree.find('synced')
-        sfile = ET.SubElement(synced, "sfile", name="Filename")
-        sfile.text = dofilename
+            #Add dofilename to sync_list.xml as incremented <key>UUID#</key>
+            #Thanks to J.F. Sebastian http://stackoverflow.com/users/4279/j-f-sebastian
+            tree = ET.parse('sync_list.xml')
+            synced = tree.find('synced')
+            sfile = ET.SubElement(synced, "sfile", date=today)
+            sfile.text = dofilename
 
-        tree.write('sync_list.xml', encoding='utf-8', xml_declaration=True)
+            tree.write('sync_list.xml', encoding='utf-8', xml_declaration=True)
+            #Thanks Martijn Pieters http://stackoverflow.com/users/100297/martijn-pieters
+            processed += 1
 
-        print 'Synced ' + dofilename + '....>'
+            print 'Synced ' + dofilename + '....>'
 
     #END FOR EACH LOOP
 print 'done!'
